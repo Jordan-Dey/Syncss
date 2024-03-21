d02643c8a200e6ca343aab4a4ecea5f218513f78
cchampeau <cchampeau@kaliop.com>
2024-03-21 11:35:35 +0100

    v.0.1.7: updated class names based on kfrontend guidelines, renamed grid layout file, fixed 2 typos

--- modules/grid-layout/install.js
+++ modules/grid-layout/install.js
@@ -29,7 +29,7 @@ export function init(projectConfig) {
     // Import files into _index.scss
     const newsImportPart = [
       '// grid-layout module',
-      '@use "./component/grid-layout.scss";',
+      '@use "./component/GridLayout.scss";',
     ].join("\n");
     appendFile(destinationIndexPath, newsImportPart);
   }
